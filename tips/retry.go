dc2d1a3522890ee16d9a68df1b450ede5c449bdd
felis.est <101798325+felisest@users.noreply.github.com>
2024-11-06 14:40:29 +0300

    Update retry.go

--- tips/retry.go
+++ tips/retry.go
@@ -3,30 +3,58 @@ package main
 import (
 	"errors"
 	"fmt"
+	"reflect"
 	"time"
 )
 
-func DoRetryOne[T any](retry int, duration time.Duration, fn func(T) error, fnArgs ...any) error {
+func DoRetryReflect(retry int, duration int, fn interface{}, args ...interface{}) error {
+	fnValue := reflect.ValueOf(fn)
+	argsValue := make([]reflect.Value, len(args))
+	for i, arg := range args {
+		argsValue[i] = reflect.ValueOf(arg)
+	}
+
+	var err error
 	for retryCount := 0; retryCount < retry; retryCount++ {
 
-		time.Sleep(duration * time.Duration(retryCount))
+		time.Sleep(time.Millisecond * time.Duration(duration*retryCount))
 
-		err := fn(fnArgs[0].(T))
+		result := fnValue.Call(argsValue)
+
+		err = (result[0].Interface()).(error)
 
 		if err == nil {
 			return nil
 		}
 	}
 
-	return nil
+	return err
 }
 
-func DoRetryTwo[T0 any, T1 any](retry int, duration time.Duration, fn func(T0, T1) error, fnArgs ...any) error {
+func DoRetryOne[T any](retry int, duration int, fn func(T) error, fnArgs ...any) error {
 
+	var err error
 	for retryCount := 0; retryCount < retry; retryCount++ {
-		time.Sleep(duration * time.Duration(retryCount))
 
-		err := fn(
+		time.Sleep(time.Millisecond * time.Duration(duration*retryCount))
+
+		err = fn(fnArgs[0].(T))
+
+		if err == nil {
+			return nil
+		}
+	}
+
+	return err
+}
+
+func DoRetryTwo[T0 any, T1 any](retry int, duration int, fn func(T0, T1) error, fnArgs ...any) error {
+
+	var err error
+	for retryCount := 0; retryCount < retry; retryCount++ {
+		time.Sleep(time.Millisecond * time.Duration(duration*retryCount))
+
+		err = fn(
 			fnArgs[0].(T0),
 			fnArgs[1].(T1),
 		)
@@ -36,7 +64,7 @@ func DoRetryTwo[T0 any, T1 any](retry int, duration time.Duration, fn func(T0, T
 		}
 	}
 
-	return nil
+	return err
 }
 
 func testExecute(s string) error {
@@ -46,7 +74,12 @@ func testExecute(s string) error {
 }
 
 func main() {
-	fmt.Println("Hello")
 
-	DoRetryOne(3, time.Millisecond*1000, testExecute, "test Do")
+	fmt.Println("WO reflection")
+	err := DoRetryOne(3, 3000, testExecute, "test Do [T]")
+
+	fmt.Println("With reflection")
+	err = DoRetryReflect(3, 3000, testExecute, "test Do reflect") // Если передадим аргументы неверного типа - упадем во время выполнения !!!
+
+	_ = err
 }
